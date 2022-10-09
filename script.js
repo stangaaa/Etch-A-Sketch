7826c8a6651a28832b0f58417ced165f3ac0a96d
stangaaa <giostanga@gmail.com>
2022-10-09 03:50:18 +0200

    added js algorithm to color the boxes not working yet, needs to be fixed

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const grid = document.querySelector('#grid');
 const block = document.createElement('div');
-let a;
-for(i = 0; i<256; i++){
-    a = block.cloneNode(true);
-    grid.append(a);
-    a.classList.add('grid-item');
+const a = [];
+block.classList.add('grid-item')
+
+//create the grid and allocates the blocks inside a[]
+for(i=0; i<256; i++){
+    a[i] = block.cloneNode(true);
+    grid.append(a[i]);
+    a[i].classList.add('grid-item');
 }
-console.log(grid)
+
+
+//create an event listner for each element of a[]
+for(j=0; j<a.length; j++){
+    a[j].addEventListener('click', function(){
+            a[j/*fixed to 256 for some reason*/].classList.add('red')  
+    })
+}
+//there is no fucking reason why this code don't work
